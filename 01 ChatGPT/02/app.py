de4231ed7b9b6612cf372706bd4a7c285da0dd5b
Michal Radomski <michalra@op.pl>
2024-02-14 02:11:30 +0100

    Course - commit 16

--- 01 ChatGPT/02/app.py
+++ 01 ChatGPT/02/app.py
@@ -2,14 +2,18 @@ import os
 import openai
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env file
+load_dotenv()  # Take environment variables from .env file
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 # print("OPENAI_API_KEY:", OPENAI_API_KEY)
 
+user_input = input("Add your prompt: ")
+
 completion = openai.chat.completions.create(
   model="gpt-3.5-turbo",
-  messages=[{"role": "user", "content": "Hello!"}]
+  messages=[{"role": "user", "content": user_input}],
+  temperature=0.2,
+  max_tokens=1000
 )
 
-print(completion.choices[0].message)
+print(completion.choices[0].message.content, completion.usage, completion.model)
