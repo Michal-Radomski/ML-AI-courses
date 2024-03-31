e6b690764fa62b75ce1b636e12f46c70b222f49d
Michal Radomski <michalra@op.pl>
2024-03-31 19:06:42 +0200

    ML with JS - commit 05

--- 02 ML_JS/000/index.ts
+++ 02 ML_JS/000/index.ts
@@ -1,20 +1,41 @@
 import _ from "lodash";
 
-const numbersArray = [
-  [10, 5],
-  [17, 2],
-  [34, 1],
-  [60, -5],
+// const numbersArray = [
+//   [10, 5],
+//   [17, 2],
+//   [34, 1],
+//   [60, -5],
+// ];
+
+// const sortedArrays = _.sortBy(numbersArray, (row) => row[1]);
+// console.log("sortedArrays:", sortedArrays);
+
+// const mappedArrays = _.map(sortedArrays, (row) => row[1]);
+// console.log("mappedArrays:", mappedArrays);
+
+// const mappedArrays2 = _.chain(numbersArray)
+//   .sortBy((row) => row[1])
+//   .map((row) => row[1])
+//   .value();
+// console.log("mappedArrays2:", mappedArrays2);
+
+//* KNN Algorithm
+const outputs = [
+  [10, 0.5, 16, 1],
+  [200, 0.5, 16, 4],
+  [350, 0.5, 16, 4],
+  [600, 0.5, 16, 5],
 ];
 
-const sortedArrays = _.sortBy(numbersArray, (row) => row[1]);
-console.log("sortedArrays:", sortedArrays);
+const predictionPoint = 300;
 
-const mappedArrays = _.map(sortedArrays, (row) => row[1]);
-console.log("mappedArrays:", mappedArrays);
+function distance(point: number): number {
+  return Math.abs(point - predictionPoint);
+}
 
-const mappedArrays2 = _.chain(numbersArray)
-  .sortBy((row) => row[1])
-  .map((row) => row[1])
+const calculatedRes = _.chain(outputs)
+  .map((row) => [distance(row[0]), row[3]])
+  .sortBy((row) => row[0])
+  .slice(0, 3)
   .value();
-console.log("mappedArrays2:", mappedArrays2);
+console.log("calculatedRes:", calculatedRes);
