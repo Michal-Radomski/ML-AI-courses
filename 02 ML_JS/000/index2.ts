b106dfcd97431b7a27473a1eb55852de70f0b087
Michal Radomski <michalra@op.pl>
2024-04-01 12:36:25 +0200

    ML with JS - commit 18

--- 02 ML_JS/000/index2.ts
+++ 02 ML_JS/000/index2.ts
@@ -1,10 +1,16 @@
 import * as tf from "@tensorflow/tfjs-node";
 // console.log("tf:", tf);
 
-const data = tf.tensor([1, 2, 3]);
+const data = tf.tensor([
+  [1, 2, 3],
+  [4, 5, 6],
+]);
 // console.log("data:", data);
 // console.log("data.shape:", data.shape);
-const data2 = tf.tensor([4, 5, 6]);
+const data2 = tf.tensor([
+  [4, 5, 6],
+  [1, 2, 3],
+]);
 const data3 = data.add(data2);
 data3.print();
 
