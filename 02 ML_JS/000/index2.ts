59852cb95388132304da68c2084b53cb28ad6da2
Michal Radomski <michalra@op.pl>
2024-04-01 12:33:50 +0200

    ML with JS - commit 17

--- 02 ML_JS/000/index2.ts
+++ 02 ML_JS/000/index2.ts
@@ -7,3 +7,10 @@ const data = tf.tensor([1, 2, 3]);
 const data2 = tf.tensor([4, 5, 6]);
 const data3 = data.add(data2);
 data3.print();
+
+const data4 = data.sub(data2);
+data4.print();
+const data5 = data.mul(data2);
+data5.print();
+const data6 = data.div(data2);
+data6.print();
