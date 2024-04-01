d88f5abece70b8bf666a48be00f934368b5c2a2a
Michal Radomski <michalra@op.pl>
2024-04-02 01:18:08 +0200

    ML with JS - commit 26

--- 02 ML_JS/000/index3.ts
+++ 02 ML_JS/000/index3.ts
@@ -1,5 +1,13 @@
 import * as tf from "@tensorflow/tfjs-node";
 
+// const test = [{ value: 20 }, { value: 30 }, { value: 15 }, { value: 5 }];
+// const testSorted = test.sort((a, b) => a.value - b.value);
+// console.log(testSorted); //* [ { value: 5 }, { value: 15 }, { value: 20 }, { value: 30 } ]
+// const testSorted2 = test.sort((a, b) => {
+//   return a.value > b.value ? 1 : -1;
+// });
+// console.log(testSorted2); //* [ { value: 5 }, { value: 15 }, { value: 20 }, { value: 30 } ]
+
 const features = tf.tensor([
   [-121, 47],
   [-121.2, 46.5],
@@ -13,3 +21,11 @@ const predictionPoint = tf.tensor([[-121, 47]]);
 
 const distances = features.sub(predictionPoint).pow(2).sum(1).pow(0.5);
 distances.print();
+
+const tsData = distances
+  .expandDims(1)
+  .concat(labels, 1)
+  .unstack()
+  .sort((a, b) => (a.arraySync() as number[])[0] - (b.arraySync() as number[])[0]);
+
+console.log("tsData:", tsData);
