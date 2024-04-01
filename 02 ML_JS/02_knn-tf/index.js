1f7a77d97b88c9ea316e1e08aa9cc272c6f61c20
Michal Radomski <michalra@op.pl>
2024-04-02 01:50:42 +0200

    ML with JS - commit 28

---
+++ 02 ML_JS/02_knn-tf/index.js
@@ -0,0 +1,15 @@
+require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs");
+
+const loadCSV = require("./load-csv");
+
+let { features, labels, testFeatures, testLabels } = loadCSV("kc_house_data.csv", {
+  shuffle: true,
+  splitTest: 10,
+  dataColumns: ["lat", "long", "sqft_lot", "sqft_living"],
+  labelColumns: ["price"],
+});
+
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+console.log({ features }, { labels });
