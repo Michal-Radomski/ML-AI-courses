d4d330ba09d81c69141987e1d5e252abb86475e3
Michal Radomski <michalra@op.pl>
2024-04-05 01:12:35 +0200

    ML with JS - commit 39

--- 02 ML_JS/03_regressions/linear-regression/linear-regression.ts
+++ 02 ML_JS/03_regressions/linear-regression/linear-regression.ts
@@ -1,13 +1,26 @@
 import * as tf from "@tensorflow/tfjs-node";
 
+interface Options {
+  iterations: number;
+}
+
 class LinearRegression {
   features: number[][];
   labels: number[][];
-  options: {};
-  constructor(features: number[][], labels: number[][], options: {}) {
+  options: Options;
+  constructor(features: number[][], labels: number[][], options: Options) {
     this.features = features;
     this.labels = labels;
-    this.options = options;
+    this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options);
+  }
+
+  train() {
+    for (let i = 0; i < this.options.iterations; i++) {
+      this.gradientDescent();
+    }
+  }
+  gradientDescent() {
+    console.log("gradientDescent");
   }
 }
 
