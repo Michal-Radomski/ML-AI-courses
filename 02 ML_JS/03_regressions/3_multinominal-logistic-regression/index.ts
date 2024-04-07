856fe0294109a49f421eb27d6f4ff4434d924bab
Michal Radomski <michalra@op.pl>
2024-04-08 00:13:20 +0200

    ML with JS - commit 75 - multinominal logistic regression initial commit

---
+++ 02 ML_JS/03_regressions/3_multinominal-logistic-regression/index.ts
@@ -0,0 +1,33 @@
+// const plot = require("node-remote-plot");
+
+const loadCSV = require("../load-csv");
+import MultinominalLogisticRegression from "./multinominal-logistic-regression";
+
+let {
+  features,
+  labels,
+  testFeatures,
+  testLabels,
+}: { features: number[][]; labels: number[][]; testFeatures: number[][]; testLabels: number[][] } = loadCSV("./cars.csv", {
+  shuffle: true,
+  splitTest: 50, //* Usually ~50%
+  dataColumns: ["horsepower", "displacement", "weight"],
+  labelColumns: ["passed_emissions"],
+  converters: {
+    passed_emissions: (value: string) => {
+      return value === "TRUE" ? 1 : 0;
+    },
+  },
+});
+// console.log({ features, labels, testFeatures, testLabels });
+
+const multinominalLogisticRegression = new MultinominalLogisticRegression(features, labels, {
+  learningRate: 0.5,
+  iterations: 100,
+  batchSize: 50,
+  decisionBoundary: 0.5,
+});
+
+multinominalLogisticRegression.train();
+
+console.log(multinominalLogisticRegression.test(testFeatures, testLabels));
