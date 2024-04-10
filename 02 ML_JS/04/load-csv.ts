cd5593a03afaf3faf700c0f08d75f880160f910c
Michal Radomski <michalra@op.pl>
2024-04-11 00:16:19 +0200

    ML with JS - commit 89

--- 02 ML_JS/04/load-csv.ts
+++ 02 ML_JS/04/load-csv.ts
@@ -13,30 +13,33 @@ function extractColumns(data: any, columnNames: any) {
 
 export const loadCSV = (
   filename: string,
-  { dataColumns = [], labelColumns = [], converters = {}, shuffle = false, splitTest = false }
+  { dataColumns = [], labelColumns = [], converters = {} as any, shuffle = false, splitTest = false }
 ) => {
   const data = fs.readFileSync(filename, { encoding: "utf-8" }) as string;
 
   const data2 = _.map(data.split("\n"), (d) => d.split(",")) as string[][];
-  console.log("data2:", data2);
+  // console.log("data2:", data2);
 
-  // data = _.dropRightWhile(data, (val) => _.isEqual(val, [""])) as any;
-  // const headers = _.first(data);
+  const data3 = _.dropRightWhile(data2, (val) => _.isEqual(val, [""]));
+  const headers = _.first(data3) as string[];
+  // console.log("headers:", headers);
 
-  // data = _.map(data, (row, index) => {
-  //   if (index === 0) {
-  //     return row;
-  //   }
-  //   return _.map(row, (element, index) => {
-  //     if (converters[headers[index]]) {
-  //       const converted = converters[headers[index]](element);
-  //       return _.isNaN(converted) ? element : converted;
-  //     }
+  const data4 = _.map(data3, (row, index) => {
+    if (index === 0) {
+      return row;
+    }
+    return _.map(row, (element, index) => {
+      if (converters[headers[index]]) {
+        const converted = converters[headers[index]](element);
+        // console.log("converted:", converted);
+        return _.isNaN(converted) ? element : converted;
+      }
 
-  //     const result = parseFloat(element.replace('"', ""));
-  //     return _.isNaN(result) ? element : result;
-  //   });
-  // }) as any;
+      const result = parseFloat(element.replace('"', ""));
+      return _.isNaN(result) ? element : result;
+    });
+  });
+  console.log("data4:", data4);
 
   // let labels = extractColumns(data, labelColumns);
   // data = extractColumns(data, dataColumns);
