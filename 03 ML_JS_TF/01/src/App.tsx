6c35468949052f9320cadebd274c5493004d3c10
Michal Radomski <michalra@op.pl>
2024-04-11 23:59:35 +0200

    Machine Learning in Javascript with TensorFlow - commit 03

--- 03 ML_JS_TF/01/src/App.tsx
+++ 03 ML_JS_TF/01/src/App.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import * as tf from "@tensorflow/tfjs";
 
 import "./App.scss";
 
+interface CustomTensor extends tf.Tensor<tf.Rank> {
+  print: () => void;
+}
+
 const App = (): JSX.Element => {
+  // console.log("tf.version:", tf.version);
+
+  // Define a model for linear regression.
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+  model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
+
+  // Generate some synthetic data for training.
+  const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+  const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+  // Train the model using the data.
+  model.fit(xs, ys, { epochs: 10 }).then(() => {
+    // Use the model to do inference on a data point the model hasn't seen before:
+    const output = model.predict(tf.tensor2d([5], [1, 1])) as CustomTensor;
+    output.print();
+  });
+
   return (
     <React.Fragment>
       <h1 className="text-center py-3">Hello Tensorflow</h1>
