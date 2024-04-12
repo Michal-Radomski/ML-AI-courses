ce9ed130d85f23e4bf783299ca67105cce259858
Michal Radomski <michalra@op.pl>
2024-04-13 00:47:47 +0200

    Machine Learning in Javascript with TensorFlow - commit 08

--- 03 ML_JS_TF/01/node/index.ts
+++ 03 ML_JS_TF/01/node/index.ts
@@ -4,18 +4,28 @@ import * as tf from "@tensorflow/tfjs-node";
 // console.log("tf.getBackend():", tf.getBackend());
 
 // Define a simple model.
-const model = tf.sequential();
-model.add(tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] }));
-model.add(tf.layers.dense({ units: 1, activation: "linear" }));
-model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
+// const model = tf.sequential();
+// model.add(tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] }));
+// model.add(tf.layers.dense({ units: 1, activation: "linear" }));
+// model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
 
-const xs = tf.randomNormal([100, 10]);
-const ys = tf.randomNormal([100, 1]);
+// const xs = tf.randomNormal([100, 10]);
+// const ys = tf.randomNormal([100, 1]);
 
-// Train the model.
-model.fit(xs, ys, {
-  epochs: 100,
-  callbacks: {
-    onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log?.loss}`),
-  },
-});
+// // Train the model.
+// model.fit(xs, ys, {
+//   epochs: 100,
+//   callbacks: {
+//     onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log?.loss}`),
+//   },
+// });
+
+const scalar = tf.scalar(87);
+const scalar2 = tf.tensor(87);
+const scalar3 = tf.tensor([87]);
+
+scalar.print();
+scalar2.print();
+scalar3.print();
+
+tf.tensor1d([1, 2, 3]).print();
