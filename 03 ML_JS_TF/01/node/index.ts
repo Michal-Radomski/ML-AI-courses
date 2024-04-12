096ca64651b20056189b6042d13be22a076405a0
Michal Radomski <michalra@op.pl>
2024-04-13 01:17:58 +0200

    Machine Learning in Javascript with TensorFlow - commit 11

--- 03 ML_JS_TF/01/node/index.ts
+++ 03 ML_JS_TF/01/node/index.ts
@@ -62,6 +62,25 @@ const c1 = tf.tensor2d([
   [4, 5, 6],
 ]);
 
+//* Tensor operations
 a1.dot(b1).print(); // or tf.dot(a, b)
 b1.dot(a1).print();
 b1.dot(c1).print();
+
+//* Memory management
+const y = tf.tidy(() => {
+  // a, b, and one will be cleaned up when the tidy ends.
+  const one = tf.scalar(1);
+  const a = tf.scalar(2);
+  const b = a.square();
+
+  console.log("numTensors (in tidy): " + tf.memory().numTensors);
+
+  // The value returned inside the tidy function will return through the tidy, in this case to the variable y.
+  return b.add(one);
+});
+
+console.log("numTensors (outside tidy): " + tf.memory().numTensors);
+y.print();
+
+console.log("tf.memory():", tf.memory());
