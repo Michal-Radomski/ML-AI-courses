2cdeda288183e3f71577093e1d1279b6d44684ca
Michal Radomski <michalra@op.pl>
2024-04-13 18:25:38 +0200

    Machine Learning in Javascript with TensorFlow - commit 15

---
+++ 03 ML_JS_TF/01/src/Interfaces.d.ts
@@ -0,0 +1,28 @@
+interface CsvData {
+  bathrooms: 1;
+  bedrooms: 3;
+  condition: 3;
+  date: string;
+  floors: number;
+  grade: number;
+  id: number;
+  lat: number;
+  long: number;
+  price: number;
+  sqft_above: number;
+  sqft_basement: number;
+  sqft_living: number;
+  sqft_living15: number;
+  sqft_lot: number;
+  sqft_lot15: number;
+  view: number;
+  waterfront: number;
+  yr_built: number;
+  yr_renovated: number;
+  zipcode: number;
+}
+
+interface CsvLocal {
+  x: number;
+  y: number;
+}
--- 03 ML_JS_TF/01/src/Plot.tsx
+++ 03 ML_JS_TF/01/src/Plot.tsx
@@ -2,18 +2,35 @@ import React from "react";
 import * as tf from "@tensorflow/tfjs";
 
 const Plot = (): JSX.Element => {
+  const [csvData, setCsvData] = React.useState<CsvLocal[]>([]);
+
   React.useEffect(() => {
-    (async function run() {
+    (async function takeCsvData() {
       const csvUrl = "/data.csv";
       const csvData = await tf.data.csv(csvUrl, {});
       // console.log("csvData:", csvData);
-      const data = await csvData.take(10);
-      const dataArray = await data.toArray();
-      console.log("dataArray:", dataArray);
+      const data = await csvData.take(-1);
+      const dataArray = (await data.toArray()) as unknown as CsvData[];
+      // console.log("dataArray:", dataArray);
+
+      const points: CsvLocal[] = await dataArray.map((elem: CsvData) => {
+        return {
+          x: elem.sqft_living,
+          y: elem.price,
+        };
+      });
+      // console.log("points:", points);
+      setCsvData(points);
     })();
   }, []);
 
-  return <React.Fragment>Plot</React.Fragment>;
+  React.useEffect(() => {
+    if (csvData && csvData.length) {
+      console.log("csvData:", csvData);
+    }
+  }, [csvData]);
+
+  return <React.Fragment>{csvData?.length}</React.Fragment>;
 };
 
 export default Plot;
